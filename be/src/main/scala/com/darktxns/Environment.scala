de0374684d1883a105be0c42f744979fe6fc5df7
Ali <ali.rac200@gmail.com>
2016-12-18 08:03:38 +0500

    fixed compile error w/ implicit formats in Environment.scala

--- be/src/main/scala/com/darktxns/Environment.scala
+++ be/src/main/scala/com/darktxns/Environment.scala
@@ -2,15 +2,18 @@ package com.darktxns
 
 import com.amazonaws.auth.{AWSCredentials, BasicAWSCredentials}
 import com.darktxns.io.Reader
-import org.json4s._
-import org.json4s.native.JsonMethods._
+import org.json4s.DefaultFormats
+import org.json4s.native.JsonMethods
 
 object Environment
 {
     def build():Environment =
     {
         val json = Reader.readResource("config.json")
-        val config = parse(json).extract[Config]
+
+        implicit val formats = DefaultFormats
+
+        val config = JsonMethods.parse(json).extract[Config]
 
         val creds = new BasicAWSCredentials(config.awsAccessKey, config.secretKey)
         new Environment(creds)
