d93560d18a4555cbaf0c121c5a72088e05a786a1
Ali <ali.rac200@gmail.com>
2016-12-20 04:19:40 +0500

    printing transactions of first 5 blocks, though only first txn is getting printed

--- be/src/main/scala/com/darktxns/blockchain/BlockchainReader.scala
+++ be/src/main/scala/com/darktxns/blockchain/BlockchainReader.scala
@@ -7,6 +7,8 @@ import com.google.bitcoin.params.MainNetParams
 import com.google.bitcoin.utils.BlockFileLoader
 import com.google.common.collect.Lists
 
+import scala.util.control.Breaks._
+
 class BlockchainReader
 {
     val params = new MainNetParams
@@ -16,7 +18,7 @@ class BlockchainReader
     println("Loading blockchain....")
     val loader = new BlockFileLoader(params, files)
 
-    for (i <- 0 to 4)
+    for (i <- 0 to 400)
     {
         val block = loader.next()
 
@@ -27,7 +29,18 @@ class BlockchainReader
             println(s"Txn: ${txn.getHashAsString}, ${txn.getPurpose}, ${txn.getSigOpCount}")
             println(s"Appears in: ${txn.getAppearsInHashes}")
             txn.getInputs.forEach(i => println(s"Input: ${i.toString}"))
-            txn.getOutputs.forEach(o => println(s"Output: ${o.toString}"))
+            txn.getOutputs.forEach(o =>
+            {
+                println(s"Output: ${o.getValue}, ${o.getSpentBy}")
+                if (o.getScriptPubKey == null || o.getScriptPubKey.getChunks == null)
+                    break
+
+                if (o.getScriptPubKey.isSentToAddress || o.getScriptPubKey.isSentToP2SH)
+                    println(s"To: ${o.getScriptPubKey.getToAddress(params)}")
+
+                println(s"from: ${o.getScriptPubKey.getFromAddress(params)}")
+                println(s"Amount: ${o.getValue}")
+            })
         })
     }
 }
